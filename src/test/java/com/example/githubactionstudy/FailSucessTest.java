1835305bd800cac908a80df01066674f76cb9894
박세준 <tpwns6693@gmail.com>
2023-01-14 12:49:38 +0900

    add : git actions 테스트를 위한 테스트 코드 추가 - fail 케이스와 sucess 케이스가 같이 있는 경우

---
+++ src/test/java/com/example/githubactionstudy/FailSucessTest.java
@@ -0,0 +1,55 @@
+package com.example.githubactionstudy;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class FailSucessTest {
+    @Test
+    @DisplayName("테스트 실패 - 1")
+    public void git_actions테스트_실패() throws Exception {
+        //given
+        Integer test = 1;
+        Integer expect = 10;
+        //when
+        test += 1;
+        //then
+        Assertions.assertThat(test).isEqualTo(expect);
+    }
+
+    @Test
+    @DisplayName("테스트 실패 - 2")
+    public void git_actions테스트_실패2() throws Exception {
+        //given
+        Integer test = 1;
+        Integer expect = 10;
+        //when
+        test += 1;
+        //then
+        Assertions.assertThat(test).isEqualTo(expect);
+    }
+
+    @Test
+    @DisplayName("테스트 성공 - 1")
+    public void git_actions테스트_성공() throws Exception {
+        //given
+        Integer test = 1;
+        Integer expect = 2;
+        //when
+        test += 1;
+        //then
+        Assertions.assertThat(test).isEqualTo(expect);
+    }
+
+    @Test
+    @DisplayName("테스트 성공 - 2")
+    public void git_actions테스트_성공2() throws Exception {
+        //given
+        Integer test = 1;
+        Integer expect = 2;
+        //when
+        test += 1;
+        //then
+        Assertions.assertThat(test).isEqualTo(expect);
+    }
+}
