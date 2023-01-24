ddd6985203fc3ebbec92cec3f6ea147eba72d34d
park-se-jun <tpwns6693@gmail.com>
2023-01-25 04:52:31 +0900

    application.properties를 docker image 외부에서 넣어주기 위한 실습 코드 작성

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM amazoncorretto:17-alpine
+# ENV TEST_ENV="default_value123123"
+ARG JAR_FILE=build/libs/*.jar
+COPY ${JAR_FILE} app.jar
+EXPOSE 8080
+ENTRYPOINT ["java","-jar","/app.jar","--spring.config.location=file:/config/application.properties"]
--- src/main/java/com/example/githubactionstudy/TestController.java
+++ src/main/java/com/example/githubactionstudy/TestController.java
@@ -1,13 +1,17 @@
 package com.example.githubactionstudy;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${test.env}")
+    private String envTestStr;
     @GetMapping
     public String testing(){
-        return "hello world";
+        return envTestStr;
     }
 }
